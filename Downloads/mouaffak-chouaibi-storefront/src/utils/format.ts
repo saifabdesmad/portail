0a30efe7507042ff726c1d3e1fd49da8d58779c6
Saif Abdessamad <153119318+saifabdesmad@users.noreply.github.com>
2026-05-10 02:57:07 +0100

    Delete Downloads/mouaffak-chouaibi-storefront directory

--- Downloads/mouaffak-chouaibi-storefront/src/utils/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatPrice = (price: number): string => {
-  return `${price.toFixed(3)} TND`;
-};
-
-export const formatDiscount = (original: number, current: number): number => {
-  return Math.round(((original - current) / original) * 100);
-};
